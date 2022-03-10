08a82a4037305d763430a6abf4aafc8d46e5a743
SahilJodhawat <sahiljodhawat419@gmail.com>
2022-03-10 11:04:50 +0530

    committed

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val txt:TextView=findViewById(R.id.txt)
-        txt.text="Hellllo"
+        val editText:EditText=findViewById(R.id.edt_txt)
+        val snd_btn:Button=findViewById(R.id.snd_btn)
+        snd_btn.setOnClickListener {
+            intent = Intent(this@MainActivity, SecondActivity::class.java)
+            val value:String=editText.text.toString()
+            intent.putExtra("value",value)
+            startActivity(intent)
+        }
 
 
     }
