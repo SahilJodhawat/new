66ec7ad8ae17c86dfee701731d07eb739f917135
SahilJodhawat <sahiljodhawat419@gmail.com>
2022-03-10 10:13:57 +0530

    committed

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txt:TextView=findViewById(R.id.txt)
+        txt.text="Hellllo"
+
+
     }
 }
